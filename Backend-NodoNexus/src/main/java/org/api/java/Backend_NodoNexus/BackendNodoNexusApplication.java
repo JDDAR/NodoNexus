66ea924b21c8b306e1047036193847e7cec82804
JDAAR <jdanaconar@gmail.com>
2025-01-02 20:37:45 -0500

    Organizando proyecto para empezar la implementación

---
+++ Backend-NodoNexus/src/main/java/org/api/java/Backend_NodoNexus/BackendNodoNexusApplication.java
@@ -0,0 +1,13 @@
+package org.api.java.Backend_NodoNexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNodoNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNodoNexusApplication.class, args);
+	}
+
+}
---
+++ Backend-NodoNexus/src/test/java/org/api/java/Backend_NodoNexus/BackendNodoNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.java.Backend_NodoNexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNodoNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
